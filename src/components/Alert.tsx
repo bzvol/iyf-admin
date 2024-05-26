6344a0784dccfc3d334011e42e8505beb05f6fa2
bzvol <zvolenszki.benjamin@gmail.com>
2024-05-26 14:54:18 +0200

    fix alert icon size

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,13 @@
 import './styles/Alert.scss';
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {CheckCircle, Error, Info, Warning} from "@mui/icons-material";
 import {CircularProgress} from "@mui/material";
 
 type AlertType = "warning" | "error" | "info" | "success" | "loading";
 
 export default function Alert({type = "warning", children}: { type?: AlertType, children: React.ReactNode }) {
+    const [iconSize, setIconSize] = useState(0);
+
     useEffect(() => {
         // Match icon's size to the alert's height
         const alert = document.querySelector(".Alert__content") as HTMLDivElement;
@@ -13,19 +15,21 @@ export default function Alert({type = "warning", children}: { type?: AlertType, 
 
         if (!alert || !icon) return;
 
-        if (type !== "loading") icon.style.fontSize = `min(${alert.clientHeight}px, 2.5rem)`;
-        else icon.style.width = icon.style.height = `min(${alert.clientHeight}px, 2.5rem)`;
+        const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+        const size = Math.min(alert.clientHeight, 2.5 * rem);
+        setIconSize(size);
+        icon.style.width = icon.style.height = `${size}px !important`;
     }, [type]);
 
     return (
         <div className={`Alert Alert-${type}`} role="alert">
-            <AlertIcon type={type}/>
+            <AlertIcon type={type} size={iconSize}/>
             <div className="Alert__content">{children}</div>
         </div>
     );
 }
 
-function AlertIcon({type}: { type: AlertType }) {
+function AlertIcon({type, size}: { type: AlertType, size: number }) {
     switch (type) {
         case "warning":
             return <Warning className="Alert__icon"/>;
@@ -36,6 +40,6 @@ function AlertIcon({type}: { type: AlertType }) {
         case "success":
             return <CheckCircle className="Alert__icon"/>;
         case "loading":
-            return <CircularProgress thickness={5} color="inherit" className="Alert__icon"/>;
+            return <CircularProgress thickness={5} color="inherit" className="Alert__icon" size={size}/>;
     }
 }
