7352b8989dfcf5aa88d0f02caba8cea396cbb7d5
bzvol <zvolenszki.benjamin@gmail.com>
2024-07-10 08:40:56 +0200

    useNotifications, useTrigger hooks

---
+++ src/utils.ts
@@ -0,0 +1,34 @@
+import {Notification, NotificationsContext} from "./components/sidebar/Notifications";
+import {useContext} from "react";
+
+export function useNotifications() {
+    const {setNotifications} = useContext(NotificationsContext);
+    return (notification: Notification) => setNotifications(prev => [notification, ...prev]);
+}
+
+class Trigger {
+    value = false;
+
+    trigger() {
+        this.value = !this.value;
+    }
+
+    awaiter(action: () => Promise<void>): () => Promise<void> {
+        return async () => {
+            await action();
+            this.trigger();
+        }
+    }
+}
+
+export function createTrigger(): Trigger {
+    return new Trigger();
+}
+
+export function useTrigger(trigger: Trigger) {
+    return [trigger.trigger, trigger.awaiter];
+}
+
+export function useTriggerListener(trigger: Trigger) {
+    return trigger.value;
+}
